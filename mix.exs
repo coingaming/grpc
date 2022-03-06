fc264d06b9281296fdc279bccc6bcda5cf362854
Reio Piller <reio@heathmont.net>
2022-03-06 23:30:02 +0200

    fix: protobuf upgrade

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule GRPC.Mixfile do
 
   defp deps do
     [
-      {:protobuf, "~> 0.8"},
+      {:protobuf, "~> 0.9"},
       {:cowboy, "~> 2.7"},
       {:gun, "~> 2.0-rc"},
       # 2.9.0 fixes some important bugs, so it's better to use ~> 2.9.0
